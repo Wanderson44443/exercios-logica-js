dfcdfd1ea1a3cc5ed5fdc3238f2d33f259e110fa
Wanderson44443 <nunesantana.99@gmail.com>
2022-03-03 20:15:15 -0300

    criação da interface js
    javajs

--- 001/script.js
+++ 001/script.js
@@ -1,4 +1,15 @@
-function alo (){
-console.log("Alo,mundo")
+function alo(){
+document.querySelector("#resultado").value
+="Alo Mundo"
 }
-alo()
+/*saida de dados no corpo do navegaador:
+document.write("alo mundo")
+
+em uma (div),(span),(h1), etc:
+document.querySelector("#resultado").
+innerHTML="alo mundo"
+
+em um campo de texto:
+document.querySelector("#resultado").value
+="Alo mundo"
+*/
